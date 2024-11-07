5aff3e8453d4d98fa9f1f80d00bf66aeaea94d33
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-07 23:46:57 +0530

    add ability to search for given pattern in files

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,19 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
+	"strings"
 
+	"github.com/nullsploit01/cc-grep/internal"
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cc-grep",
+	Use:   "ccgrep [pattern] [file]",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -22,9 +21,30 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 2 {
+			cmd.Help()
+			os.Exit(1)
+		}
+
+		pattern := args[0]
+
+		file, err := os.Open(args[1])
+		if err != nil {
+			cmd.PrintErrln("Error opening file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		g := internal.NewGrep()
+		matches, err := g.Grep(pattern, file)
+		if err != nil {
+			cmd.PrintErrln("Error running grep:", err)
+			os.Exit(1)
+		}
+
+		cmd.OutOrStdout().Write([]byte(strings.Join(matches, "\n") + "\n"))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +67,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- internal/grep.go
+++ internal/grep.go
@@ -1,5 +1,11 @@
 package internal
 
+import (
+	"bufio"
+	"os"
+	"regexp"
+)
+
 type Grep struct {
 }
 
@@ -7,7 +13,32 @@ func NewGrep() *Grep {
 	return &Grep{}
 }
 
-func (g *Grep) Grep() {
+func (g *Grep) Grep(pattern string, file *os.File) ([]string, error) {
+	if _, err := file.Seek(0, 0); err != nil {
+		return nil, err
+	}
+
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var matches []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		if re.MatchString(line) {
+			matches = append(matches, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return matches, nil
 }
 
 func (g *Grep) RecursiveGrep() {
